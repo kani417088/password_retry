c1411049dd51c8f12e02bdf4ed2255ba9ba812c9
Natasha Lin <natasha.cy.lin@viewsonic.com>
2023-09-20 10:54:13 +0800

    version 3

---
+++ password3.py
@@ -0,0 +1,15 @@
+password = 'a123456'
+x = 3
+while x > 0:
+	x -= 1
+	pwd = input('請輸入密碼:')
+	if pwd == password:
+		print('登入成功!')
+		break
+	else:
+		print('密碼錯誤!')
+		if x > 0:
+			print('還有', x, '次機會')
+		else:
+			print('帳號鎖住, 請聯繫系統管理員')
+
